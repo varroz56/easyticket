3e57d20c668b6eef29228a1fd834468fb3705334
asd <varroz56@gmail.com>
2020-10-26 18:08:25 +0000

    howtos serializer and view

---
+++ backend/howtos/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import HowTo
+
+
+class HowToSerializer(serializers.ModelSerializer):
+    """ Serialize How To data"""
+    class Meta:
+        model = HowTo
+        fields = ('question', 'answer')
--- backend/howtos/views.py
+++ backend/howtos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView
+from .models import HowTo
+from serializers import HowToSerializer
 
-# Create your views here.
+
+class HowToListView(ListAPIView):
+    """ This view will list How-Tos ordered by the most
+    visited ones and then alphabetical """
+    permission_classes = (permissions.AllowAny,)
+    queryset = HowTo.objects.order_by('question', 'number_of_views')
+    serializer_class = HowToSerializer
