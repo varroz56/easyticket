063dca004a30a5bbffb432fe64a76295a8173382
asd <varroz56@gmail.com>
2020-10-26 12:47:43 +0000

    created contactusmessage model and custom methods

--- backend/marketing/models.py
+++ backend/marketing/models.py
@@ -15,3 +15,57 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class ContactUsMessage(models.Model):
+    """To store the message and to create the response to that message"""
+    subject = models.CharField(max_length=150)
+    message = models.TextField()
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    contact = models.CharField(max_length=254)
+
+    reference = models.CharField(max_length=10, null=True, blank=True)
+    # Create custom save method to add reference
+    # refernce is limited to 9999999 any more instance created after this
+    # it will add a backup prefix and have another 9999999 instance time to prepare
+    # with a better soultion
+    # This is to have a consistent and limited length of reference numbers
+
+    def create_reference(self):
+        # setting end and prefix vars
+        end = str(self.id)
+        prefix = "CUF"
+        # in the case of over 9999999 insatnces
+        if (self.id > 9999999):
+            # backup prefix and the end will roll back to start
+            # from 1
+            prefix = "FUC"
+            end = str(self.id-9999999)
+        # get the length of the id
+        # fill with 0s while get the required length
+        num = len(end)
+        mid = ""
+        while(num < 7):
+            mid += "0"
+            num += 1
+        # set ref from the required tags
+        ref = prefix + mid + end
+        # update the instance
+        ContactUsMessage.objects.filter(
+            id=self.id).update(reference=ref)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super().save(*args, **kwargs)
+            self.create_reference()
+
+    def get_contact(self):
+        if not self.contact:
+            return str("No contact has been linked to this message yet.")
+        return self.contact
+
+    def __str__(self):
+        if not self.reference:
+            return str("No Reference has been created yet")
+        return self.reference
