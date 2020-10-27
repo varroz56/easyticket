922d626662965663ae68f94eccd50fff0647930b
asd <varroz56@gmail.com>
2020-10-27 17:42:19 +0000

    CRUD on shippingaddress

--- backend/premium/urls.py
+++ backend/premium/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ShippingAddressCRUDView, ShippingAddressDeletView
+
+from .views import (ShippingAddressCRUDView, ShippingAddressDeletView,
+                    ShippingAddressReadView)
 
 urlpatterns = [
     path('manage-shipping-address/', ShippingAddressCRUDView.as_view()),
     path('delete-shipping-address/', ShippingAddressDeletView.as_view()),
+    path('show-shipping-address/', ShippingAddressReadView.as_view()),
 ]
--- backend/premium/views.py
+++ backend/premium/views.py
@@ -1,14 +1,16 @@
+import json
 # User will need to be authneticated in
 # order to make changes to the shipping address
 from rest_framework import permissions, status
 # generic APIView from rest
 from rest_framework.views import APIView
 # to be able to handle delete
-from rest_framework.generics import DestroyAPIView
+from rest_framework.generics import DestroyAPIView, RetrieveAPIView
 # HTTP response to handle view exits
 from rest_framework.response import Response
 # import our models
 from .models import ShippingAddress
+from .serilaizers import ShippingAddressSerializer
 from accounts.models import UserProfile
 
 
@@ -60,12 +62,39 @@ class ShippingAddressCRUDView(APIView):
         content = {'success', 'Address has been updated'}
         return Response(content, status=status.HTTP_200_OK)
 
-        # DELETE
+
+class ShippingAddressReadView(RetrieveAPIView):
+    """ This view to handle get request on
+        It will exit if no user, no address or
+        when found
+    """
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        # using the data came with the request
+        data = self.request.data
+
+        # try to find user if not found error
+        user = UserProfile.objects.filter(id=data['user'])
+        if not user:
+            content = {'error', 'User not found'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        user = UserProfile.objects.get(id=data['user'])
+
+        address = ShippingAddress.objects.filter(user=data['user'])
+        if not address:
+            content = {
+                'info', 'No Address has been saved yet, unable to show'}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        queryset = ShippingAddress.objects.get(user=data['user'])
+        serializer = ShippingAddressSerializer(queryset, many=False)
+        content = {'success', 'Address found'}
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ShippingAddressDeletView(DestroyAPIView):
     """ This view to handle instance deletion
-        It will exit if no user, no address or 
+        It will exit if no user, no address or
         when the instance deleted
     """
     permission_classes = (permissions.AllowAny,)
