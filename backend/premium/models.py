d06ec39c2cf8e7bdc5be4ef864e64705e9cd06ec
asd <varroz56@gmail.com>
2020-10-28 09:08:15 +0000

    cleaned up create_unique_slug

--- backend/premium/models.py
+++ backend/premium/models.py
@@ -4,23 +4,6 @@ from accounts.models import UserProfile
 from django.template.defaultfilters import slugify
 
 
-class ShippingAddress(models.Model):
-    """ User's shipping address One to one relation to user"""
-    user = models.OneToOneField(
-        "accounts.UserProfile", on_delete=models.CASCADE)
-    address_line_one = models.CharField(max_length=254)
-    address_line_two = models.CharField(max_length=254)
-    postcode = models.CharField(max_length=10)
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name_plural = "Shipping Adresses"
-
-    def __str__(self):
-        return self.user.email
-
-
 class PremiumPackage(models.Model):
     """ Premium Package name and data to show """
     name = models.CharField(max_length=50)
@@ -39,7 +22,7 @@ class PremiumPackage(models.Model):
              So a record can be kept about same package changes
         """
         myslug = slugify(self.name)
-        print("creating unique slug from "+myslug)
+        # print("creating unique slug from "+myslug)
         # As probably the same main name will be used like "30 day", "6 month",
         #  annual the next filter is outside of the if statement, however,
         # this is unecessary when the slug is unique
@@ -48,51 +31,31 @@ class PremiumPackage(models.Model):
         # more effective in this case
         ppackages = PremiumPackage.objects.filter(slug__startswith=myslug)
         if ppackages:
-            print("Found same slug start")
-            if len(ppackages) == 1:
-                print("Only one same slug start: " + ppackages.first().slug)
-                if ppackages.first().slug == myslug:
-                    myslug += "-1"
-                    PremiumPackage.objects.filter(
-                        id=self.id).update(slug=myslug)
-                    print("Set instance slug to: " + myslug)
-                else:
-                    print(
-                        "Found same slug start but not same slug, using slugified name")
-                    PremiumPackage.objects.filter(
-                        id=self.id).update(slug=myslug)
-                    print("Set instance slug to: " + myslug)
-
-            else:
-
-                print("Number of same slug starts:")
-                print(len(ppackages))
-                n = 1
-                while n < len(ppackages)+1:
-                    print(
-                        "going through all packages with same slug start.." + str(n) + "st/th time(s)")
-                    is_unique = True
-                    while is_unique:
-                        for p in ppackages:
-                            myslug = slugify(self.name+"-"+str(n))
-                            p.slug
-                            print("My slug : " +
-                                  myslug+" Comparing to: " + p.slug)
-                            if myslug == p.slug:
-                                print(myslug +
-                                      " is not unique as it is equal to: " + p.slug)
-                                is_unique = False
-                                n += 1
-                        if is_unique:
-                            PremiumPackage.objects.filter(
-                                id=self.id).update(slug=myslug)
-                            print("Set instance slug to: " + myslug)
+            n = 1
+            while n < len(ppackages)+1:
+                # print(
+                #     "going through all packages with same slug start.." + str(n) + "st/th time(s)")
+                is_unique = True
+                while is_unique:
+                    for p in ppackages:
+                        myslug = slugify(self.name+"-"+str(n))
+                        p.slug
+                        # print("My slug : " +
+                        #       myslug+" Comparing to: " + p.slug)
+                        if myslug == p.slug:
+                            # print(myslug +
+                            #       " is not unique as it is equal to: " + p.slug)
                             is_unique = False
-                            n = len(ppackages)+1
-
+                            n += 1
+                    if is_unique:
+                        PremiumPackage.objects.filter(
+                            id=self.id).update(slug=myslug)
+                        # print("Set instance slug to: " + myslug)
+                        is_unique = False
+                        n = len(ppackages)+1
         else:
             PremiumPackage.objects.filter(id=self.id).update(slug=myslug)
-            print("Default slug is unique, set instance slug to: " + myslug)
+            # print("Default slug is unique, set instance slug to: " + myslug)
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -101,3 +64,20 @@ class PremiumPackage(models.Model):
 
     def __str__(self):
         return self.slug
+
+
+class ShippingAddress(models.Model):
+    """ User's shipping address One to one relation to user"""
+    user = models.OneToOneField(
+        "accounts.UserProfile", on_delete=models.CASCADE)
+    address_line_one = models.CharField(max_length=254)
+    address_line_two = models.CharField(max_length=254)
+    postcode = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Shipping Adresses"
+
+    def __str__(self):
+        return self.user.email
