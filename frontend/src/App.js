b09cb4914720ff184a634286f028850165468a38
asd <varroz56@gmail.com>
2020-10-30 09:17:27 +0000

    created basic structure plan

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,27 @@
 function App() {
   return (
     <div className="App">
-      REACT APP
+      <table className="table border">
+        <th>Layout</th>
+        <tr>That decides which case and load the containers and components</tr>
+        <th>
+          <table className="table border">
+            <th>Navbar</th>
+            <tr>The Options can be selected without authentication</tr>
+            <tr>The Options can only be selected with authentication</tr>
+          </table>
+          <table className="table border">
+            <th>Main Content</th>
+            <tr>The pages can be reached without authentication</tr>
+            <tr>The pages can only be reached with authentication</tr>
+          </table>
+          <table className="table border">
+            <th>Footer</th>
+            <tr>The Options can be selected without authentication</tr>
+            <tr>The Options can only be selected with authentication</tr>
+          </table>
+        </th>
+      </table>
     </div>
   );
 }
