5a7d80074b5b2250da502020c677bde1c2b9e291
asd <varroz56@gmail.com>
2020-10-30 22:21:41 +0000

    style and components restructured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './hocs/Layout';
 
-import About from './components/guestPages/About';
-import HowTo from './components/guestPages/HowTo';
-import Premium from './components/guestPages/Premium';
-import ContactUs from './components/guestPages/ContactUs';
+import About from './components/homePages/About';
+import HowTo from './components/howtos/HowTo';
+import Premium from './components/premium/Premium';
+import ContactUs from './components/marketing/ContactUs';
 
-import Home from './components/guestPages/Home';
-import Login from './components/authPages/Login';
-import SignUp from './components/authPages/SignUp';
-import Activate from './components/authPages/Activate';
-import ResetPassword from './components/authPages/PasswordReset';
-import ConfirmResetPassword from './components/authPages/PasswordReset';
+import Home from './components/homePages/Home';
+import Login from './components/accounts/Login';
+import SignUp from './components/accounts/SignUp';
+import Activate from './components/accounts/Activate';
+import ResetPassword from './components/accounts/PasswordReset';
+import ConfirmResetPassword from './components/accounts/PasswordReset';
 
-import CheckoutForm from './components/userPages/Checkout';
+import CheckoutForm from './components/payments/Checkout';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
