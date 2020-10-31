a87038262232fa700165de13f6f3a3f6b072e338
asd <varroz56@gmail.com>
2020-10-31 05:52:16 +0000

    imported our store to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import './sass/main.scss';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Provider to make our store availabel for
+// nested compoonents  and import our store
+import { Provider } from 'react-redux';
+import store from './store';
 import Layout from './hocs/Layout';
 
 import About from './components/homePages/About';
@@ -30,41 +34,44 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/howto" component={HowTo} />
-        <Route exact path="/premium" component={Premium} />
-        <Route exact path="/contactus" component={ContactUs} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/events" component={Events} />
-        <Route exact path="/select-event" component={SelectEvent} />
-        <Route exact path="/submit-event" component={SubmitEvent} />
-        <Route exact path="/event-details" component={EventDetails} />
-        <Route
-          exact
-          path="/reset_password"
-          component={ResetPassword}
-        />
-        <Route
-          exact
-          path="/password/reset/confirm/:uid/:token"
-          component={ConfirmResetPassword}
-        />
-        <Route
-          exact
-          path="/activate/:uid/:token"
-          component={Activate}
-        />
-        <Elements stripe={stripePromise}>
-          <Route exact path="/checkout" component={CheckoutForm} />
-        </Elements>
-      </Switch>
-    </Layout>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/howto" component={HowTo} />
+          <Route exact path="/premium" component={Premium} />
+          <Route exact path="/contactus" component={ContactUs} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/events" component={Events} />
+          <Route exact path="/select-event" component={SelectEvent} />
+          <Route exact path="/submit-event" component={SubmitEvent} />
+          <Route exact path="/event-details" component={EventDetails} />
+          <Route
+            exact
+            path="/reset_password"
+            component={ResetPassword}
+          />
+          <Route
+            exact
+            path="/password/reset/confirm/:uid/:token"
+            component={ConfirmResetPassword}
+          />
+          <Route
+            exact
+            path="/activate/:uid/:token"
+            component={Activate}
+          />
+          <Elements stripe={stripePromise}>
+            <Route exact path="/checkout" component={CheckoutForm} />
+          </Elements>
+        </Switch>
+      </Layout>
+    </Router>
+
+  </Provider>
 );
 
 export default App;
